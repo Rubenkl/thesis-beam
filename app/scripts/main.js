47162d04ebd49ada616f2a2806b52034d8d70bd1
Ruben K <ruben.kluge@student.ru.nl>
2017-02-01 17:21:14 +0100

    Now working tracking gyroscope and accelerometer

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,36 @@
 console.log('\'Allo \'Allo!');
+
+var alpha, beta, gamma;
+var ax, ay, az;
+
+var state = {};
+
+
+
+window.addEventListener('devicemotion', function(e) {
+  ax = e.accelerationIncludingGravity.x;
+  ay = e.accelerationIncludingGravity.y;
+  az = e.accelerationIncludingGravity.z;
+});
+
+window.ondeviceorientation = function(e) {
+  alpha = Math.round(event.alpha);
+  beta = Math.round(event.beta);
+  gamma = Math.round(event.gamma);
+};
+
+startDataStream()
+
+
+function startDataStream() {
+  setInterval(function() {
+    $('#ga').text('Alpha: ' + alpha);
+    $('#gb').text('Beta: ' + beta);
+    $('#gg').text('Gamma: ' + gamma);
+
+    $('#ax').text('X: ' + Math.round(Math.abs(ax)));
+    $('#ay').text('Y: ' + Math.round(Math.abs(ay)));
+    $('#az').text('Z: ' + Math.round(Math.abs(az)));
+
+  }, 1000 / 20);
+}
