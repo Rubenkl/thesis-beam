8b0f356987f6631526d95eb96cd789d3e3dd70a4
Ruben K <ruben.kluge@student.ru.nl>
2017-03-07 16:27:09 +0100

    continuous classifying first start, already watching the folders!

--- trainer/graphing/KNN-DTWcontinuous.py
+++ trainer/graphing/KNN-DTWcontinuous.py
@@ -14,16 +14,19 @@ import os #filesystem reads
 from tinylearn import KnnDtwClassifier
 from tinylearn import CommonClassifier
 
+from helpers import FolderWatch #custom module, yay!
 
 
 DATA_FOLDER = "../data/"
+CLASSIFY_FOLDER = "../data/CLASSIFY/"
 
 
 # Helper functions:
 
+
 # get all the data files from the directory
-def getDataFileNames(dataType, movement = ""):
-  files = os.listdir(DATA_FOLDER);
+def getDataFileNames(dataType, movement = "", dataFolder = DATA_FOLDER):
+  files = os.listdir(dataFolder);
   output = []
   for file in files:
     if dataType in file and movement in file:
@@ -68,12 +71,33 @@ for trainingFile in files:
 print("label size:", len(training_data))
 print("data size:", len(training_labels))
 
+clf1 = KnnDtwClassifier(1)
+clf1.fit(training_data, training_labels)
+
 
 
 # -- testing --
+global previousFile
+previousFile = "none"
+
+def classify(classiFile):
+  global previousFile
+  if (previousFile == "none"):
+    previousFile = classiFile
+    FolderWatch.FolderWatch(CLASSIFY_FOLDER, classify)
+  else:
+    print("classifying: " + previousFile)
+
+# CONTINUE WITH THE TESTINT SHIT ITSELF HERE!!
+
+
+FolderWatch.FolderWatch(CLASSIFY_FOLDER, classify)
+
 
 test_data =[]
 test_labels =[]
+
+'''
 
 files = getDataFileNames("test")
 for trainingFile in files:
@@ -97,86 +121,10 @@ for trainingFile in files:
 print("label size:", len(test_data))
 print("data size:", len(test_labels))
 
-'''
 
-#original_headers = list(dataFile.columns.values)
-N = 198
-
-time = np.linspace(0,20, 198)
-
-
-#importing data
-
-
-dataFile = pd.read_csv("../data/training-updown-RbWE5pRKUdnyeTTAAAI-1.csv", header =0)
-dataA = dataFile['beta'].values
-
-dataFileB = pd.read_csv("../data/training-updown-RbWE5pRKUdnyeTTAAAI-2.csv", header=0)
-dataB = dataFileB['beta'].values
-
-dataFileC = pd.read_csv("../data/training-leftright-RbWE5pRKUdnyeTTAAAI-3.csv", header=0)
-dataC = dataFileC['beta'].values
-
-dataFileF = pd.read_csv("../data/training-leftright-RbWE5pRKUdnyeTTAAAI-4.csv", header=0)
-dataF = dataFileF['beta'].values
-
-dataFileD = pd.read_csv("../data/training-updown-RbWE5pRKUdnyeTTAAAI-7.csv", header=0)
-dataD = dataFileD['beta'].values
-
-dataFileE = pd.read_csv("../data/training-updown-RbWE5pRKUdnyeTTAAAI-6.csv", header=0)
-dataUpdownTest = dataFileE['beta'].values
-
-dataFileQuick = pd.read_csv("../data/training-updown-mg8f0Om0lSmaYI9AAAP-1-QUICK.csv", header=0)
-dataQuick = dataFileQuick['beta'].values
-
-dataTestLeftRight = pd.read_csv("../data/training-leftright-RbWE5pRKUdnyeTTAAAI-8.csv", header=0)
-dataLeftrightTest = dataTestLeftRight['beta'].values
-
-
-#adding data arrays to a new array for training
-training_data = np.array([dataA, dataB, dataC, dataF, dataD, dataQuick])
-training_labels = np.array(['updown', 'updown', 'leftright', 'leftright', 'updown', 'updown'])
-test_data = [dataUpdownTest, dataLeftrightTest]
-test_labels = ['updown', 'leftright']
-
-'''
-
-
-clf1 = KnnDtwClassifier(1)
-clf1.fit(training_data, training_labels)
 
 
 for index, t in enumerate(test_data):
   print("KNN-DTW prediction for " + str(test_labels[index]) + " = " + str(clf1.predict(t)))
 
-#plotting:
-
-for i in range (0, 35, 5):
-    hist, bins = np.histogram(training_data[i], bins=20)
-    width = 0.7 * (bins[1] - bins[0])
-    center = (bins[:-1] + bins[1:]) / 2
-    plt.title(training_labels[i])
-    plt.bar(center, hist, align='center', width=width)
-    plt.show()
-
-
-
-#fdataD = fftpack.fft(dataA)
-
-#distance, path = fastdtw(dataA, dataQuick)
-
-#print(distance)
-
-
-
-# from: https://raw.githubusercontent.com/markdregan/K-Nearest-Neighbors-with-Dynamic-Time-Warping/master/K_Nearest_Neighbor_Dynamic_Time_Warping.ipynb
-'''
-fig = plt.figure(figsize=(12,4))
-_ = plt.plot(time, dataA, label='A')
-_ = plt.plot(time, dataQuick, label='Quick')
-_ = plt.title('DTW distance between A and Quick is %.2f' % distance)
-_ = plt.ylabel('Amplitude')
-_ = plt.xlabel('Time')
-_ = plt.legend()
-plt.show()
 '''
