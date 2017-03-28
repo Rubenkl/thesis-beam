83da5ca1095d97212c9a08c56f3e6bbd7dcf7099
Ruben K <ruben.kluge@student.ru.nl>
2017-03-28 14:48:53 +0200

    Tried to implement SVM, but multidimensional seemed to fail

---
+++ trainer/graphing/SVM.py
@@ -0,0 +1,125 @@
+import docopt
+import numpy as np
+import pandas as pd
+import matplotlib.pylab as plt
+import itertools
+from sklearn import svm
+
+from sklearn.pipeline import Pipeline
+from sklearn.svm import SVC
+from sklearn.decomposition import PCA
+from sklearn.model_selection import GridSearchCV
+
+import os #filesystem reads
+
+#for supervised hmm shit
+
+from helpers import DataAnalyzer
+
+DATA_FOLDER = "../data/"
+CLASSIFY_FOLDER = "C:/Users/Ruben/Dropbox/Coding/GIT/Thesis/trainer/data/CLASSIFY"
+
+
+# Helper functions:
+
+
+# get all the data files from the directory
+def getDataFileNames(dataType, movement = "", dataFolder = DATA_FOLDER):
+  files = os.listdir(dataFolder);
+  output = []
+  for file in files:
+    if dataType in file and movement in file:
+      output.append(file)
+  return output
+
+
+
+# ------------------- MAIN ------------------------------------
+
+analyzer = DataAnalyzer.DataAnalyzer()
+
+# -- training --
+
+training_data = []
+training_labels = []
+training_data_length = []
+
+files = getDataFileNames("training")
+for trainingFile in files:
+  dataFile = pd.read_csv(DATA_FOLDER + trainingFile, header = 0)
+  #data = [dataFile['alpha'], dataFile['beta'], dataFile['gamma'], dataFile['accX'], dataFile['accY'], dataFile['accZ']]
+  dataFile = analyzer.normalize(dataFile)
+  dataFile = analyzer.autoCorrelate(dataFile)
+  
+  training_data.append(dataFile['accX'][:150])
+  if "updown" in trainingFile:
+    training_labels.append("updown")
+  elif "leftright" in trainingFile:
+    training_labels.append("leftright")
+  elif "rotateclock" in trainingFile:
+    training_labels.append("rotateclockwise")
+
+print("label size:", len(training_data))
+print("data size:", len(training_labels))
+
+print(training_data)
+
+
+#model = svm.SVC()
+parameters = {'kernel':('linear', 'rbf'), 'C':[1, 10]}
+
+estimators = [('reduce_dim', PCA()), ('clf', svm.SVC())]
+
+model = GridSearchCV(estimators, parameters )
+
+model.fit(training_data, training_labels)
+
+
+#----- testing -------
+
+test_data = []
+test_labels = []
+test_data_length = []
+
+
+files = getDataFileNames("test")
+for trainingFile in files:
+  dataObject = pd.read_csv(DATA_FOLDER + trainingFile, header = 0)
+  #data = [dataFile['accX'], dataFile['accY'], dataFile['accZ']]
+  #data = [dataFile['alpha'], dataFile['beta'], dataFile['gamma'], dataFile['accX'], dataFile['accY'], dataFile['accZ']]
+  dataObject = analyzer.normalize(dataObject)
+  dataObject = analyzer.autoCorrelate(dataObject)
+
+  test_data.append(dataObject['accX'][:150])
+  if "updown" in trainingFile:
+    test_labels.append("updown")
+  elif "leftright" in trainingFile:
+    test_labels.append("leftright")
+  elif "rotateclock" in trainingFile:
+    test_labels.append("rotateclockwise")
+
+
+
+print("label size:", len(test_data))
+print("data size:", len(test_labels))
+
+
+for index, data in enumerate(test_data):
+    print("AffinityProp prediction for " + str(test_labels[index]) + " = " + str(model.predict([data])))
+
+
+'''
+
+for index, t in enumerate(test_data):
+
+  test_matrix = []
+
+  for data in t:
+    row = []
+    for secondData in t:
+      row.append(analyzer.DTWSimilarity(data, secondData))
+    test_matrix.append(row)
+
+  print("AffinityProp prediction for " + str(test_labels[index]) + " = " + str(model.predict(test_matrix)))
+
+'''
--- trainer/graphing/helpers/test-DataAnalyzer.py
+++ trainer/graphing/helpers/test-DataAnalyzer.py
@@ -2,6 +2,8 @@ import DataAnalyzer
 import pandas as pd
 import matplotlib.pyplot as plt
 import Visualizer
+import numpy as np
+import itertools
 
 
 
@@ -9,6 +11,8 @@ dataFile = pd.read_csv("../../data/testing-leftright-qsMbpdsd6zQTqlrKAADi-1-72BP
 dataFile2 = pd.read_csv("../../data/training-leftright-avkfxrmpauHdDpeaAAAa-3.csv", header=0)
 dataFile3 = pd.read_csv("../../data/training-updown-avkfxrmpauHdDpeaAAAa-1.csv", header=0)
 dataFile4 = pd.read_csv("../../data/training-rotateclockwise-avkfxrmpauHdDpeaAAAa-6.csv", header=0)
+
+analyzer = DataAnalyzer.DataAnalyzer()
 
 '''
 data = dataFile['gamma'].values
@@ -32,11 +36,6 @@ output2 = analyzer.getFFTDataFromStream(analyzer.getAutocorrelation())
 print(output['detectedBPM'])
 
 '''
-
-analyzer = DataAnalyzer.DataAnalyzer()
-
-dataFile = analyzer.normalize(dataFile)
-print(dataFile)
 #dataFile2 = analyzer.normalize(dataFile2)
 #dataFile3 = analyzer.normalize(dataFile3)
 #dataFile4 = analyzer.normalize(dataFile4)
@@ -49,4 +48,36 @@ print(dataFile)
 
 #print(analyzer.autoCorrelate(dataFile2))
 
+
+training_data = [12,4,51,21,8]
+
+
+dataFile = analyzer.normalize(dataFile)
+training_matrix = []
+
+
+for i,data in enumerate(training_data):
+  row = []
+  for j, secondData in enumerate(training_data):
+    if (i==j):
+      row.append(0)
+    else:
+      row.append(data * secondData)
+  training_matrix.append(row)
+
+print(training_matrix)
+
+
+#second
+
+test_matrix = np.zeros((len(training_data),len(training_data)))
+
+for n in itertools.combinations(np.arange(len(training_data)), 2):
+
+  test_matrix[n[0], n[1]] = training_data[n[0]] * training_data[n[1]]
+  test_matrix[n[1], n[0]] = training_data[n[0]] * training_data[n[1]]
+
+print(test_matrix)
+
+print(training_matrix == test_matrix)
 
